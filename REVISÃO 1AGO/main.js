62423995592d5c949c3f98ef6eb8b44ad2f15eef
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-08-01 21:24:09 -0300

    not ready

--- REVISÃO 1AGO/main.js
+++ REVISÃO 1AGO/main.js
@@ -1,3 +1,7 @@
 function clicar(){
-    document.getElementById("clicou").innerHTML = "ELOGIOS ESGOTADOS"
+    document.getElementById("clicou").innerHTML = "<h1><b>ELOGIOS ESGOTADOS</b></h1>"
+}
+
+function passar(){
+    document.getElementById("clicou").innerHTML = "<h1><b>FALEI QUE TÁ ESGOTADO</b></h1>"
 }
